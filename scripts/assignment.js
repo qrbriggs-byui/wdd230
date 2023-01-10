99dabe0a7b50500da68341ac2c52ce288cea3f0a
qrbriggs-byui <briggsq@byui.edu>
2023-01-10 12:25:46 -0700

    Purple...

--- scripts/assignment.js
+++
@@ -1,8 +0,0 @@
-// This sets the year in the footer
-const today = new Date();
-let year = document.querySelector("#year");
-year.textContent = today.getFullYear();
-
-// This sets the last modified date in the footer
-let lastmodified = document.querySelector("#lastupdate");
-lastmodified.textContent = document.lastModified;
