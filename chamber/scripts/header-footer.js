bcc57a794d923d215509cae5109d31d84e414ae5
qrbriggs-byui <briggsq@byui.edu>
2024-05-16 15:43:21 -0600

    Week 4 Chamber updates

---
+++ chamber/scripts/header-footer.js
@@ -0,0 +1,8 @@
+let datespan = document.querySelector("#currentYear")
+datespan.innerText = new Date().getFullYear()
+
+let lastmodifiedspan = document.querySelector("#lastModified")
+lastmodifiedspan.innerText = document.lastModified
+
+
+
