7f0d1372e411264d25075ee6b45e4f7ca0e4e97c
qrbriggs-byui <briggsq@byui.edu>
2024-06-18 13:42:58 -0600

    Week 09 Stuff + site plan

---
+++ lesson09/scripts/prophets-example.js
@@ -0,0 +1,28 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+function displayProphets(prophets){
+    prophets.forEach((prophet) => {
+        let section = document.createElement("section")
+        section.classList.add("card")
+        let sectionHTML =`<h3>${prophet.name} ${prophet.lastname}</h3>
+        <p>Date of Birth: ${prophet.birthdate}</p>
+        <p>Place of Birth: ${prophet.birthplace}</p>
+        <img src="${prophet.imageurl}" alt="Picture of ${prophet.name} ${prophet.lastname}">`
+        section.innerHTML = sectionHTML;
+        cards.appendChild(section);
+    });
+}
+
+async function getProphetData(){
+    const response = await fetch(url);
+    if (response.ok){
+        const data = await response.json();
+        displayProphets(data.prophets);
+    }
+    else{
+        console.error("NO WORKIE!!!")
+    }
+}
+
+getProphetData()
