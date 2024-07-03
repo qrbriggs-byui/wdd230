18d04765379525c8f5c53e5732e35daffda9368c
qrbriggs-byui <briggsq@byui.edu>
2024-07-03 15:20:12 -0600

    Join Page

--- chamber/scripts/meet-greet.js
+++ chamber/scripts/meet-greet.js
@@ -7,3 +7,9 @@ if (dayofweek >= 1 && dayofweek <= 4){
 document.querySelector(".meet-greet button").addEventListener("click", () => {
     document.querySelector(".meet-greet").classList.remove("show")
 })
+
+function openJoinPage(){
+    window.open("join.html")
+}
+
+document.querySelector(".hero button").addEventListener("click", openJoinPage)
